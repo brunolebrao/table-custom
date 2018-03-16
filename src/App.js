e8f4743284ad3b0a85d3d32240e34a45f6b81a4f
bruno.lebrao <bruno.lebrao@socialsquad.co>
2018-03-15 22:54:20 -0300

    Infinity Scroll

--- src/App.js
+++ src/App.js
@@ -3,22 +3,109 @@ import './App.css';
 import {theadData, tbodyData} from "./data";
 
 class App extends Component {
-    render() {
 
-        console.log(tbodyData);
+    handleScroll = () => {
+
+        if (this.scroller && this.scroller.scrollTop + window.innerHeight >= this.scroller.scrollHeight) {
+            console.log(
+                'chama varias vezes depois da condição como posso parar assim que chamar a prim' +
+                'eira vez?'
+            );
+        }
+
+    };
+
+    render() {
 
         return (
             <div>
-                <table>
-                    <thead>
-                    <tr>
-                        {Object.keys(tbodyData.results[1].custom_fields).map((_,index) => <th key={index} style={{padding: 10}}>{tbodyData.results[1].custom_fields[_].name}</th>)}
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {tbodyData.results.map((_, index) => <tr key={index}>{_.custom_fields.map((t, index) => <td key={index}>{t.translated_value}</td>)}</tr>)}
-                    </tbody>
-                </table>
+                <div
+                    id='containerDiv'
+                    onScroll={this.handleScroll}
+                    ref={(scroller) => {
+                        this.scroller = scroller
+                    }}
+                    style={{
+                        width: 300,
+                        height: 200,
+                        overflow: 'auto'
+                    }}>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Teste</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                            <tr>
+                                <td>Scroll</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
